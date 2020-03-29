7d4ed9b8afad3e733b69ac259a0d0484570cf039
cevherkarakoc <cevherkarakoc@live.com>
2020-03-29 14:44:12 +0300

    SSR global sorununa geçici bir çözüm.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,15 @@ import React from 'react';
 import {Redirect} from '@docusaurus/router';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
+import GLMath from 'webgl-math';
+
+global.DOC = {
+  separator : ' , ',
+  radiansToDegrees: angle => angle * (180 / Math.PI),
+  degreesToRadians: angle => angle * (Math.PI / 180),
+};
+global.GLMath = GLMath;
+
 function Home() {
   return <Redirect to={useBaseUrl('/intro')} />;
 }
