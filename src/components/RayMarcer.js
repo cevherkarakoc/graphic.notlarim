ba323bcc3ccdc3fb3cd212e102c139876b0ae120
Cevher Karakoç <cevherkarakoc@live.com>
2021-05-23 13:59:56 +0300

    add sdf infinite cylinder

--- src/components/RayMarcer.js
+++ src/components/RayMarcer.js
@@ -7,6 +7,10 @@ const SDF_SET = {
         type : 'sdf',
         code : 'ZmxvYXQgc2RmKHZlYzMgUCkgewogIGZsb2F0IHIgPSAxLjsKICByZXR1cm4gbGVuZ3RoKFApIC0gcjsKfQ=='
     },
+    infinite_cylinder : {
+        type: 'sdf',
+        code : 'ZmxvYXQgc2RmKHZlYzMgUCkgewogIGZsb2F0IHIgPSAxLjsKICByZXR1cm4gbGVuZ3RoKFAueHopIC0gcjsKfQ=='
+    },
     union: {
         type: 'customScene',
         code : 'ZmxvYXQgc2RmRG9udXQodmVjMyBwLCB2ZWMyIHQpIHsKICB2ZWMyIHEgPSB2ZWMyKGxlbmd0aChwLnh6KS10LngscC55KTsKICByZXR1cm4gbGVuZ3RoKHEpLXQueTsKfQoKZmxvYXQgc2RmU3BoZXJlKHZlYzMgcCwgZmxvYXQgcikgewogIHJldHVybiBsZW5ndGgocCkgLSByOwp9CgpmbG9hdCBnZXRTY2VuZURpc3QodmVjMyBwKSB7CiAgZmxvYXQgZDEgPSBzZGZEb251dChwIC0gdmVjMygtMS4sIDAuLCAwLikgLHZlYzIoMi4sMC43NSkpOwogIGZsb2F0IGQyID0gc2RmU3BoZXJlKHAgLSB2ZWMzKDEuNSwgMC4sIDAuKSwgMi4pOwogIAogIGZsb2F0IGQgPSBtaW4oZDEsIGQyKTsKICAKICByZXR1cm4gZDsKfQo='
