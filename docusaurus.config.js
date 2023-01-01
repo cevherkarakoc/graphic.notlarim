8d9b44f6798a4844b09afe51239228565d273e76
cevherkarakoc <cevherkarakoc@live.com>
2023-01-01 14:06:29 +0300

    update docusaurus 2.2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,49 +1,79 @@
-module.exports = {
-  title: 'Grafik Programlama Notlarım',
-  tagline: 'Grafik Programlama Üzerine Aldığım Şahsi Notlar',
-  url: 'https://graphic.notlarim.dev',
-  baseUrl: '/',
-  favicon: 'img/favicon.ico',
-  organizationName: 'cevherkarakoc', // Usually your GitHub org/user name.
-  projectName: 'graphic.notlarim', // Usually your repo name.
-  themes: ['@docusaurus/theme-live-codeblock'],
-  themeConfig: {
-    sidebarCollapsible: true,
-    navbar: {
-      title: 'Grafik Notlarım',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          href: 'https://github.com/cevherkarakoc/graphic.notlarim',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      copyright: `CC0 1.0 Universal (CC0 1.0) Public Domain Dedication, Built with Docusaurus.`,
-    },
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Grafik Programlama Notlarım",
+  tagline: "Grafik Programlama Üzerine Aldığım Şahsi Notlar",
+  url: "https://graphic.notlarim.dev",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "cevherkarakoc", // Usually your GitHub org/user name.
+  projectName: "graphic.notlarim", // Usually your repo name.
+  trailingSlash: false,
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "tr",
+    locales: ["tr"],
   },
+  themes: ["@docusaurus/theme-live-codeblock"],
   presets: [
     [
-      '@docusaurus/preset-classic',
-      {
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          routeBasePath: '/', 
-          editUrl: 'https://github.com/cevherkarakoc/graphic.notlarim/edit/master/',
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
-      },
+      }),
     ],
   ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Grafik Notlarım",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            href: "https://github.com/cevherkarakoc/graphic.notlarim",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        copyright: `CC0 1.0 Universal (CC0 1.0) Public Domain Dedication, Built with Docusaurus.`,
+      },
+      prism: {
+        theme: darkCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
   stylesheets: [
     'https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css',
   ],
 };
+
+module.exports = config;
